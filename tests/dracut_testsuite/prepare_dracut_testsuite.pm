e0f3192a18051e3c358491585319a54b10585e2c
root <root@mcbrain-2.arch.suse.de>
2020-10-12 17:35:59 +0200

    add dracut tests

---
+++ tests/dracut_testsuite/prepare_dracut_testsuite.pm
@@ -0,0 +1,29 @@
+# SUSE's openQA tests
+#
+# Copyright © 2020 SUSE LLC
+#
+# Copying and distribution of this file, with or without modification,
+# are permitted in any medium without royalty provided the copyright
+# notice and this notice are preserved.  This file is offered as-is,
+# without any warranty.
+
+# Summary: Prepare systemd and testsuite.
+# Maintainer: Sergio Lindo Mansilla <slindomansilla@suse.com>, Thomas Blume <tblume@suse.com>
+
+use base 'dracut_testsuite_test';
+use warnings;
+use strict;
+use testapi;
+
+sub run {
+    my ($self) = @_;
+    $self->testsuiteinstall;
+    assert_script_run('cd /usr/lib/dracut/tests');
+    assert_script_run('./run-tests.sh --prepare', 600);
+}
+
+sub test_flags {
+    return {fatal => 1, milestone => 1};
+}
+
+1;
