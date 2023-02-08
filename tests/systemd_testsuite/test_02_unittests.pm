4af9273faad65b133c66e0351c2ffecf45927e19
root <root@knopfler.arch.suse.de>
2023-02-08 17:53:07 +0100

    exchange cryptsetup and unittest

--- tests/systemd_testsuite/test_02_unittests.pm
+++ tests/systemd_testsuite/test_02_unittests.pm
@@ -1,9 +1,9 @@
 # SUSE's openQA tests
 #
-# Copyright 2019-2020 SUSE LLC
+# Copyright 2019 SUSE LLC
 # SPDX-License-Identifier: FSFAP
 
-# Summary: Run test executed by TEST-04-JOURNAL from upstream after openSUSE/SUSE patches.
+# Summary: Run test executed by TEST-02-UNITTESTS from upstream after openSUSE/SUSE patches.
 # Maintainer: Sergio Lindo Mansilla <slindomansilla@suse.com>, Thomas Blume <tblume@suse.com>
 
 use base 'systemd_testsuite_test';
@@ -14,15 +14,15 @@ use testapi;
 sub pre_run_hook {
     my ($self) = @_;
     #prepare test
-    $self->testsuiteprepare('TEST-04-JOURNAL');
+    $self->testsuiteprepare('TEST-02-UNITTESTS');
 }
 
 sub run {
     #run test
-    my $timeout = get_var('SYSTEMD_TEST_DEFAULT_TIMEOUT') || 480;
+    my $timeout = 900;
     assert_script_run 'cd /usr/lib/systemd/tests/integration-tests';
-    assert_script_run 'export NO_BUILD=1 && make -C TEST-04-JOURNAL run 2>&1 | tee /tmp/testsuite.log', $timeout;
-    assert_script_run 'grep "TEST-04-JOURNAL RUN: .* \[OK\]" /tmp/testsuite.log';
+    assert_script_run 'export NO_BUILD=1 && make -C TEST-02-UNITTESTS run 2>&1 | tee /tmp/testsuite.log', $timeout;
+    assert_script_run 'grep "TEST-02-UNITTESTS RUN: .* \[OK\]" /tmp/testsuite.log';
 }
 
 sub test_flags {
--- tests/systemd_testsuite/test_24_cryptsetup.pm
+++ tests/systemd_testsuite/test_24_cryptsetup.pm
@@ -3,7 +3,7 @@
 # Copyright 2019-2020 SUSE LLC
 # SPDX-License-Identifier: FSFAP
 
-# Summary: Run test executed by TEST-04-JOURNAL from upstream after openSUSE/SUSE patches.
+# Summary: Run test executed by TEST-02-CRYPTSETUP from upstream after openSUSE/SUSE patches.
 # Maintainer: Sergio Lindo Mansilla <slindomansilla@suse.com>, Thomas Blume <tblume@suse.com>
 
 use base 'systemd_testsuite_test';
@@ -14,15 +14,16 @@ use testapi;
 sub pre_run_hook {
     my ($self) = @_;
     #prepare test
-    $self->testsuiteprepare('TEST-04-JOURNAL');
+    $self->testsuiteprepare('TEST-24-CRYPTSETUP');
 }
 
 sub run {
     #run test
-    my $timeout = get_var('SYSTEMD_TEST_DEFAULT_TIMEOUT') || 480;
+    my $timeout = 600;
     assert_script_run 'cd /usr/lib/systemd/tests/integration-tests';
-    assert_script_run 'export NO_BUILD=1 && make -C TEST-04-JOURNAL run 2>&1 | tee /tmp/testsuite.log', $timeout;
-    assert_script_run 'grep "TEST-04-JOURNAL RUN: .* \[OK\]" /tmp/testsuite.log';
+    assert_script_run 'export NO_BUILD=1 && make -C TEST-24-CRYPTSETUP run 2>&1 | tee /tmp/testsuite.log', $timeout;
+    assert_script_run 'grep "TEST-24-CRYPTSETUP RUN: .* \[OK\]" /tmp/testsuite.log';
+    script_run 'export NO_BUILD=1 && make -C TEST-24-CRYPTSETUP clean';
 }
 
 sub test_flags {
